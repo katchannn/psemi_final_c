a17657f7b9d131dcd77d5cd03a1e2c8caa38732e
Suzuki Katsuhiro <suzunoki24@gmail.com>
2023-05-10 15:26:32 +0900

    一部インクリメント

--- backend/api/cruds/news.py
+++ backend/api/cruds/news.py
@@ -9,16 +9,17 @@ PASSWORD = 'password'
 #connect to mongodb
 DATABASE_URL = f'mongodb://{USERNAME}:{PASSWORD}@{HOST}:{PORT}'
 client = MongoClient(DATABASE_URL)
-db = client["test"]
 print("Connected to MongoDB")
 
-my_collection = db["data"]
 
-data_1 = {
-    "Name":"test1",
-    "Age":25,
-}
+# db = client["test"]
+# my_collection = db["data"]
 
+# data_1 = {
+#     "Name":"test1",
+#     "Age":25,
+# }
+# my_collection.insert_one(data_1)
 
 def db_create_news(news):
-    my_collection.insert_one(news)
+    pass
