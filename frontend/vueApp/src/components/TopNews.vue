a7d21adf537e2e3f6383bcb6a6c065efde24accd
katchannn <suzunoki24@gmail.com>
2023-05-24 21:52:26 +0900

    change design

--- frontend/vueApp/src/components/TopNews.vue
+++ frontend/vueApp/src/components/TopNews.vue
@@ -1,75 +1,4 @@
 <!-- <template>
-  <v-card class="mx-auto" max-width="500">
-    <v-container fluid>
-      <v-row dense>
-        <v-col :cols="12" md="6" class="h-100">
-          <v-card
-            v-for="item in data.slice(0, 1)"
-            :key="item.id"
-            class="card"
-            @click="handleClick(item.id)"
-          >
-            <v-img
-              :src="item.imageSrc"
-              class="align-end"
-              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-              height="400px"
-              cover
-            >
-              <v-card-title class="text-white">{{ item.title }}</v-card-title>
-            </v-img>
-          </v-card>
-        </v-col>
-        <v-col :cols="6" md="6">
-          <v-row dense>
-            <v-col :cols="12">
-              <v-card
-                v-for="item in data.slice(1, 2)"
-                :key="item.id"
-                class="card"
-                @click="handleClick(item.id)"
-              >
-                <v-img
-                  :src="item.imageSrc"
-                  class="align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  height="200px"
-                  cover
-                >
-                  <v-card-title class="text-white">{{
-                    item.title
-                  }}</v-card-title>
-                </v-img>
-              </v-card>
-            </v-col>
-            <v-col :cols="12">
-              <v-card
-                v-for="item in data.slice(2, 3)"
-                :key="item.id"
-                class="card"
-                @click="handleClick(item.id)"
-              >
-                <v-img
-                  :src="item.imageSrc"
-                  class="align-end"
-                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                  height="200px"
-                  cover
-                >
-                  <v-card-title class="text-white">{{
-                    item.title
-                  }}</v-card-title>
-                </v-img>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-card>
-</template> -->
-
-<template>
   <v-carousel show-arrows="hover" cycle hide-delimiter-background>
     <v-carousel-item
       v-for="item in data.slice(0, 3)"
@@ -98,7 +27,154 @@
       </v-img>
     </v-carousel-item>
   </v-carousel>
+</template> -->
+
+<template>
+  <v-card class="mx-auto ma-0 pa-0">
+    <v-container fluid style="padding: 0%">
+      <v-row no-gutters>
+        <v-col :cols="12" md="6" class="ma-0 pa-0">
+          <v-card
+            v-for="item in data.slice(0, 1)"
+            :key="item.id"
+            @click="handleClick(item.id)"
+            class="ma-0 pa-0"
+            style="border-radius: 0"
+          >
+            <v-img
+              :src="item.img"
+              class="align-end"
+              gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+              height="500px"
+              cover
+              @click="handleClick(item.id)"
+            >
+              <v-card-title class="text-white">{{ item.title }}</v-card-title>
+            </v-img>
+          </v-card>
+        </v-col>
+        <v-col :cols="12" md="6" class="ma-0 pa-0">
+          <v-row no-gutters>
+            <v-col :cols="12" class="ma-0 pa-0">
+              <v-card
+                v-for="item in data.slice(1, 2)"
+                :key="item.id"
+                @click="handleClick(item.id)"
+                class="ma-0 pa-0"
+                style="border-radius: 0"
+              >
+                <v-img
+                  :src="item.img"
+                  class="align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="250px"
+                  cover
+                  @click="handleClick(item.id)"
+                >
+                  <v-card-title class="text-white">{{
+                    item.title
+                  }}</v-card-title>
+                </v-img>
+              </v-card>
+            </v-col>
+            <v-col :cols="12" class="ma-0 pa-0">
+              <v-card
+                v-for="item in data.slice(2, 3)"
+                :key="item.id"
+                @click="handleClick(item.id)"
+                class="ma-0 pa-0"
+                style="border-radius: 0"
+              >
+                <v-img
+                  :src="item.img"
+                  class="align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="250px"
+                  cover
+                  @click="handleClick(item.id)"
+                >
+                  <v-card-title class="text-white">{{
+                    item.title
+                  }}</v-card-title>
+                </v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
+<!-- <template>
+<v-carousel show-arrows="hover" cycle hide-delimiter-background>
+    <v-carousel-item
+      v-for="item in data.slice(0, 3)"
+      :key="item.id"
+      @click="handleClick(item.id)"
+      cover
+    >
+      <v-card class="mx-auto ma-0 pa-0">
+        <v-container fluid style="padding: 0%">
+          <v-row no-gutters>
+            <v-col :cols="12" md="6" class="ma-0 pa-0">
+              <v-card
+                class="ma-0 pa-0"
+                style="border-radius: 0"
+              >
+                <v-img
+                  :src="item.img"
+                  class="align-end"
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                  height="500px"
+                  cover
+                >
+                  <v-card-title class="text-white">{{ item.title }}</v-card-title>
+                </v-img>
+              </v-card>
+            </v-col>
+            <v-col :cols="12" md="6" class="ma-0 pa-0">
+              <v-row no-gutters>
+                <v-col :cols="12" class="ma-0 pa-0">
+                  <v-card
+                    class="ma-0 pa-0"
+                    style="border-radius: 0"
+                  >
+                    <v-img
+                      :src="item.img"
+                      class="align-end"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      height="250px"
+                      cover
+                    >
+                      <v-card-title class="text-white">{{ item.title }}</v-card-title>
+                    </v-img>
+                  </v-card>
+                </v-col>
+                <v-col :cols="12" class="ma-0 pa-0">
+                  <v-card
+                    class="ma-0 pa-0"
+                    style="border-radius: 0"
+                  >
+                    <v-img
+                      :src="item.img"
+                      class="align-end"
+                      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                      height="250px"
+                      cover
+                    >
+                      <v-card-title class="text-white">{{ item.title }}</v-card-title>
+                    </v-img>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-carousel-item>
+</v-carousel>
+</template> -->
+
 
 <script>
 import { axios } from "/app/plugins/axios";
