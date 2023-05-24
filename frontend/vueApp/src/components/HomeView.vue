4e42838d269f924d68a2382d02477f05e1df0727
Suzuki Katsuhiro <suzunoki24@gmail.com>
2023-05-24 16:01:56 +0900

    change design

--- frontend/vueApp/src/components/HomeView.vue
+++ frontend/vueApp/src/components/HomeView.vue
@@ -1,6 +1,23 @@
 <template>
-  <TopNews @news-clicked="navigateToDetail"/>
-  <ListNews @news-clicked="navigateToDetail"/>
+  <v-app-bar color="primary">
+    <v-toolbar-title class="headline text-uppercase">
+      <span class="font-weight-light">SECURITY</span>
+      <span class="font-weight-bold">NEWS AGGREGATOR</span>
+    </v-toolbar-title>
+  </v-app-bar>
+
+  <!-- <v-app-bar color="primary">
+  <v-toolbar-title>
+    <v-avatar>
+      <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjKx6lnebkMoVxrD6i2a9kHJMAK5StxF6UxajtGC-QKg5H7keNnKCBTpf-Bd8WwGeUEEfMG2Ggx08MrkhJWyUl22L9HcF5u4bQjfUVvL0VUOr0pFg3D_XL31sY-zLG7VDiFGPVTewvqYAqdOJK9m6gUKqO6V3YHg5ylRQkhbSZxgEioqOxwvUsuvejm/s1600/hackers.jpg" alt="Logo">
+    </v-avatar>
+    <span class="ml-3 title font-weight-light">Security</span>
+    <span class="title font-weight-bold">News Aggregator</span>
+  </v-toolbar-title>
+  </v-app-bar> -->
+
+  <TopNews @news-clicked="navigateToDetail" />
+  <ListNews @news-clicked="navigateToDetail" />
 </template>
 
 <script>
@@ -14,8 +31,8 @@ export default {
   },
   methods: {
     navigateToDetail(newsId) {
-      this.$router.push({ name: 'Detail', params: { id: newsId } });
-    }
-  }
+      this.$router.push({ name: "Detail", params: { id: newsId } });
+    },
+  },
 };
 </script>
