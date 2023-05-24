788e93f4b28826f16626e28f2533486a5791c0da
Suzuki Katsuhiro <suzunoki24@gmail.com>
2023-05-24 17:44:52 +0900

    change design

--- frontend/vueApp/src/App.vue
+++ frontend/vueApp/src/App.vue
@@ -1,38 +1,29 @@
 <template>
   <v-app>
     <v-main>
-
-      <!-- <v-app-bar color="primary">
-      <v-app-bar color="primary" @click="goHome" style="cursor: pointer;">
-    <v-toolbar-title class="headline text-uppercase">
-      <span class="font-weight-light">SECURITY</span>
-      <span class="font-weight-bold">NEWS AGGREGATOR</span>
-    </v-toolbar-title>
-  </v-app-bar> -->
-
-  <v-app-bar color="primary">
-  <v-toolbar-title>
-    <v-avatar>
-      <img src="/isl_icon.png" width="40" height="45">
-    </v-avatar>
-    <span class="ml-3 title font-weight-light">Security</span>
-    <span class="title font-weight-bold">News Aggregator</span>
-  </v-toolbar-title>
-  </v-app-bar>
-     <router-view />
+      <v-app-bar color="primary" @click="goHome" style="cursor: pointer">
+        <v-toolbar-title>
+          <v-avatar>
+            <img src="/isl_icon.png" width="40" height="45" />
+          </v-avatar>
+          <span class="ml-3 title font-weight-light">Security</span>
+          <span class="title font-weight-bold">News Aggregator</span>
+        </v-toolbar-title>
+      </v-app-bar>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   methods: {
     goHome() {
-      this.$router.push('/')
-    }
-  }
-}
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style>
