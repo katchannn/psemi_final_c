ba3138f688d687b088ae2e30831258ba67c9784d
katchannn <suzunoki24@gmail.com>
2023-05-27 23:06:33 +0900

    非同期処理

--- backend/api/cruds/news.py
+++ backend/api/cruds/news.py
@@ -86,14 +86,14 @@ def news_serializer(news) -> dict:
     } 
     
 
-def db_get_news(news_id) -> dict:
+async def db_get_news(news_id) -> dict:
     news = my_collection.find_one({"_id":ObjectId(news_id)})
     news = news_serializer(news)
     
     return news
 
     
-def db_get_newsList() -> list:
+async def db_get_newsList() -> list:
     newsDoc = my_collection.find().sort("_id",pymongo.DESCENDING)
     newsList = [news_serializer(news) for news in newsDoc]
     
